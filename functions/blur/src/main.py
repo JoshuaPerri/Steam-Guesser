795b44564707de51dd38abdecaec26408339738c
Josh Perri <joshuaperri1@gmail.com>
2025-12-07 02:07:12 -0500

    Test image return

--- functions/blur/src/main.py
+++ functions/blur/src/main.py
@@ -4,11 +4,13 @@ import requests
 from io import BytesIO
 
 def main(context):
-    # response = requests.get("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1030300/7983574d464e6559ac7e24275727f73a8bcca1f3/header.jpg")
+    response = requests.get("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1030300/7983574d464e6559ac7e24275727f73a8bcca1f3/header.jpg")
 
-    # with Image.open(BytesIO(response.content)) as im:
-    #     im = im.filter(ImageFilter.GaussianBlur(radius=50))
-    #     im.save("test.jpg")
+    with Image.open(BytesIO(response.content)) as im:
+        im = im.filter(ImageFilter.GaussianBlur(radius=50))
+        output = BytesIO()
+        im.save(output, format='JPEG')
+        return context.res.binary(output.getvalue())
 
     context.log(context.req.body_text)
 
@@ -22,6 +24,6 @@ def main(context):
             "motto": "Build like a team of hundreds_",
             "learn": "https://appwrite.io/docs",
             "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
+            "getInspired": "https://builtwith.appwrite.io"
         }
     )
